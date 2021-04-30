518c3cfe863d9cf048d37bf860d0d085b6ec3083
Kevin <kevalot@gmail.com>
2021-04-29 21:20:14 -0400

    add additional commands

--- TrashBot.py
+++ TrashBot.py
@@ -3,6 +3,9 @@ import youtube
 import globals
 import discord
 import random
+import re
+import uwuify
+
 
 from discord.ext import commands
 prefix = "!"
@@ -12,45 +15,13 @@ globals.initialize(commands.Bot(command_prefix=commands.when_mentioned_or("!"),
 bot = globals.bot
 deleteMessage = False
 
-
-# @bot.event
-# async def on_message(message):
-#     print("The message's content was", message.content)
-#     await bot.process_commands(message)
-
-@bot.command()
-async def h(ctx):
-    '''
-    This text will be shown in the help command
-    '''
-
-    message = str("```Availiable commands:\n" +
-    "Music: \n" +
-    "   !yt <url> play from youtube\n" +
-    "   !stream <url> stream from url\n" +
-    "   !stop stops playing and disconnects the bot\n" +
-    "   !join <channelName> Trashbot joins <channelName>\n"
-    "Other:\n" +
-    "   !bruh BRUH\n" +
-    "   !bruhs a quieter more reserved Bruh (no emoji)\n" +
-    "   !roll rolls a d20\n" +
-    "   !chooseOne chooses someone from the channel that Trashbot is currently in\n"
-    "Trash Related:\n" +
-    "   !whoIsTheTraitor who can it be?\n" +
-    "   !whoStaysCool there is only one person we can count on to stay cool" +
-    "```")
-    
-    # style = discord.Embed(name="responding quote", description="- "+message)
-    # await bot.send(embed=style)
-    await ctx.send(message)
-
 @bot.command()
 async def echo(ctx, *, content:str):
     await ctx.send(content)
     emoji = discord.utils.get(bot.emojis, name='tiltproof')
     print(emoji)
 
-@bot.command()
+@bot.command(help="who can it be?")
 async def whoIsTheTraitor(ctx):
     await ctx.send("Its Logan... maybe Emily")
 
@@ -58,19 +29,28 @@ async def whoIsTheTraitor(ctx):
 async def fuckYouMatt(ctx):
     await ctx.send("Yes Fuck you Matt")
 
-@bot.command()
+@bot.command(help="there is only one person we can count on to stay cool")
 async def whoStaysCool(ctx):
     await ctx.send("<:tiltproof:741028686651719691> Its Logan <:tiltproof:741028686651719691> <@174733787182137345>")
     # for member in ctx.message.guild.members:
     #     print(member)
     #     print(member.id)
 
-@bot.command()
+@bot.command(help="rolls a d20")
 async def roll(ctx):
     randomnumber = random.randint(1, 20)
     await ctx.send(":game_die: " + str(randomnumber) + " :game_die: ")
 
+@bot.command(help="im sorry for this abomination of a command")
+async def uwu(ctx, content:str):
+    await ctx.send(uwuify.uwu(content))
+
 @bot.command()
+async def noAnime(ctx):
+    await ctx.send('https://media.tenor.com/images/5fd4212ffb6b24df9c186be39719223e/tenor.gif')
+    ctx.message.delete()
+
+@bot.command(help="chooses someone from the channel that Trashbot is currently in")
 async def chooseOne(ctx):
     print(ctx.voice_client)
     # if ctx.voice_client is None:
@@ -89,8 +69,34 @@ async def on_ready():
     print('Logged in as {0} ({0.id})'.format(bot.user))
     print('------')
 
+@bot.event
+async def on_message(message):
+    # print(message)
+    if (message.author.bot == False):
+        if(message.author.id == 174733787182137345):
+            emoji = '\N{THUMBS UP SIGN}'
+            await message.add_reaction(emoji)
+            emoji = '\N{THUMBS DOWN SIGN}'
+            await message.add_reaction(emoji)
+        if(message.author.id == 174731616801914880): 
+            #harass matt
+            randomnumber = random.randint(1, 10)
+            if randomnumber == 1:
+                emoji = bot.get_emoji(583877834552901632)
+                await message.add_reaction(emoji)
+            if re.search('trashbot', message.content, re.IGNORECASE):
+                emoji = bot.get_emoji(583877834552901632)
+                await message.add_reaction(emoji)
+            if re.search('trash', message.content, re.IGNORECASE) and re.search('bot', message.content, re.IGNORECASE):
+                emoji = bot.get_emoji(583877834552901632)
+                await message.add_reaction(emoji)
+        if re.search('owie', message.content, re.IGNORECASE):
+            emoji = bot.get_emoji(813783762017583139)
+            await message.add_reaction(emoji)
+    await bot.process_commands(message)
+    
+
 youtube.bot = bot
 youtube.bot.add_cog(youtube.Music(bot))
 
-youtube.bot.run('<>')  # Where 'TOKEN' is your bot token
-
+youtube.bot.run('')  # Where 'TOKEN' is your bot token
--- youtube.py
+++ youtube.py
@@ -1,9 +1,9 @@
 import asyncio
-
 import discord
 import youtube_dl
 import ffmpeg
 import globals
+import threading
 
 from discord.ext import commands
 
@@ -55,6 +55,12 @@ class YTDLSource(discord.PCMVolumeTransformer):
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    
+    @classmethod
+    async def leavetimer(self, ctx):
+        self.ctx = ctx
+        await asyncio.sleep(1200)
+        await self.ctx.voice_client.disconnect()
 
     async def joinDefault(self, ctx):
         """Joins a voice channel"""
@@ -62,22 +68,31 @@ class Music(commands.Cog):
             channel = discord.utils.get(ctx.message.guild.channels, name="Test", type="ChannelType.voice")
             print(channel)
             if channel is None:
-                channel = discord.utils.get(ctx.message.guild.channels, name="*teleports behind you*", type="ChannelType.voice")
+                for ch in ctx.message.guild.channels:
+                    if(ch.id == 420394425014026255):
+                        channel = ch
+                # channel = discord.utils.get(ctx.message.guild.channels, name="*teleports behind you*", type="ChannelType.voice")
+                # channel = discord.utils.get(ctx.message.guild.channels, id=420394425014026254)
             print(channel)
             if channel is not None: 
                 print("connecting")
                 await channel.connect()
+                await self.leavetimer(ctx)
 
     @commands.command()
+    async def leave(self, ctx):
+        await ctx.voice_client.disconnect()
+
+    @commands.command(help="<channelName> Trashbot joins <channelName>")
     async def join(self, ctx, *, channel: discord.VoiceChannel):
         """Joins a voice channel"""
 
         if ctx.voice_client is not None:
             await ctx.voice_client.move_to(channel)
 
-        await ctx.message.delete()
         await channel.connect()
         await self.hi(ctx)
+        await self.leavetimer(ctx)
 
     @commands.command()
     async def play(self, ctx, *, query):
@@ -88,12 +103,12 @@ class Music(commands.Cog):
 
         await ctx.send('Now playing: {}'.format(query))
 
-    @commands.command()
+    @commands.command(help="<url> play from youtube")
     async def yt(self, ctx, *, url):
         try:
             """Plays from a url (almost anything youtube_dl supports)"""
 
-            self.joinDefault(ctx)
+            await self.joinDefault(ctx)
 
             async with ctx.typing():
                 player = await YTDLSource.from_url(url, loop=self.bot.loop)
@@ -104,11 +119,27 @@ class Music(commands.Cog):
         except:
             print("An exception occurred")
 
-    @commands.command()
+    @commands.command(help="<url> play from youtube")
+    async def stop(self, ctx, *, url):
+        try:
+            """Plays from a url (almost anything youtube_dl supports)"""
+
+            await self.joinDefault(ctx)
+
+            async with ctx.typing():
+                player = await YTDLSource.from_url(url, loop=self.bot.loop)
+                ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+                await ctx.message.delete()
+                await ctx.send('Now playing: {}'.format(player.title))
+        except:
+            print("An exception occurred")
+
+    @commands.command(help="<url> stream from url")
     async def stream(self, ctx, *, url):
         try:
             """Streams from a url (same as yt, but doesn't predownload)"""
-            self.joinDefault(ctx)
+            await self.joinDefault(ctx)
 
             async with ctx.typing():
                 player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
@@ -130,16 +161,23 @@ class Music(commands.Cog):
         ctx.voice_client.source.volume = volume / 100
         await ctx.send("Changed volume to {}%".format(volume))
 
-    @commands.command()
+    @commands.command(help="stops playing and disconnects the bot")
     async def stop(self, ctx):
         """Stops and disconnects the bot from voice"""
+        try:
+            async with ctx.typing():
+                source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("bye.webm"))
+                ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
+
         await ctx.message.delete()
         await ctx.voice_client.disconnect()
 
     @commands.command()
     async def bruh(self, ctx):
         try:
-            self.joinDefault(ctx)
+            await self.joinDefault(ctx)
             async with ctx.typing():
                 player = await YTDLSource.from_url("https://www.youtube.com/watch?v=2ZIpFytCSVc", loop=self.bot.loop)
                 ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
@@ -151,10 +189,10 @@ class Music(commands.Cog):
             print("An exception occurred")
         
     
-    @commands.command()
+    @commands.command(help="a quieter more reserved Bruh (no emoji)")
     async def bruhs(self, ctx):
         try:
-            self.joinDefault(ctx)
+            await self.joinDefault(ctx)
             async with ctx.typing():
                 player = await YTDLSource.from_url("https://www.youtube.com/watch?v=2ZIpFytCSVc", loop=self.bot.loop)
                 ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
@@ -163,25 +201,151 @@ class Music(commands.Cog):
         except:
             print("An exception occurred")
 
-    @commands.command(pass_context=True)
+    @commands.command(pass_context=True, help="hello... trash!")
     async def hi(self, ctx):
         try:
-            self.joinDefault(ctx)
+            await self.joinDefault(ctx)
             async with ctx.typing():
                 player = await YTDLSource.from_url("https://sound.peal.io/ps/audios/000/007/769/original/youtube_7769.mp3?1517524109", loop=self.bot.loop)
                 ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
         except:
             print("An exception occurred")
+        
+    @commands.command(pass_context=True)
+    async def bye(self, ctx):
+        try:
+            await self.joinDefault(ctx)
+            async with ctx.typing():
+                source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("bye.webm"))
+                ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
 
     @commands.command()
     async def whoisit(self, ctx):
-        """Plays a file from the local filesystem"""
         try:
             source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("C:\\Users\\keval\\OneDrive\\Desktop\\DiscBot\\Hello who is it.mp3"))
             ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
         except:
             print("An exception occurred")
         await ctx.message.delete()
+
+    @commands.command()
+    async def owie(self, ctx):
+        try:
+            await self.joinDefault(ctx)
+            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("C:\\Users\\keval\\OneDrive\\Desktop\\DiscBot\\owie.mp3"))
+            ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
+
+    @commands.command()
+    async def beanswtf(self, ctx):
+        try:
+            await self.joinDefault(ctx)
+            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("C:\\Users\\keval\\OneDrive\\Desktop\\DiscBot\\beanswtf.mp3"))
+            ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
+        
+    @commands.command(help="owie full version")
+    async def owiel(self, ctx):
+        try:
+            await self.joinDefault(ctx)
+            async with ctx.typing():
+                player = await YTDLSource.from_url("https://www.youtube.com/watch?v=e1pYQQKY6Gs", loop=self.bot.loop)
+                ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
+
+    @commands.command()
+    async def no(self, ctx):
+        try:
+            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("C:\\Users\\keval\\OneDrive\\Desktop\\DiscBot\\no.m4a"))
+            ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
+        await ctx.message.delete()
+    
+    @commands.command(pass_context=True)
+    async def playingFactorio(self, ctx):
+        try:
+            await self.joinDefault(ctx)
+            async with ctx.typing():
+                player = await YTDLSource.from_url("https://www.youtube.com/watch?v=b6SODkIzbn0", loop=self.bot.loop)
+                ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
+
+    @commands.command(pass_context=True)
+    async def loveJesus(self, ctx):
+        try:
+            await self.joinDefault(ctx)
+            async with ctx.typing():
+                player = await YTDLSource.from_url("https://www.youtube.com/watch?v=Vl6L8BDC_eg", loop=self.bot.loop)
+                ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
+
+    @commands.command(pass_context=True)
+    async def baba(self, ctx):
+        try:
+            await self.joinDefault(ctx)
+            async with ctx.typing():
+                player = await YTDLSource.from_url("https://www.youtube.com/watch?v=n7uc6W9qqVs", loop=self.bot.loop)
+                ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
+
+    @commands.command(pass_context=True, help="why thou?")
+    async def spicy(self, ctx):
+        try:
+            await self.joinDefault(ctx)
+            async with ctx.typing():
+                player = await YTDLSource.from_url("https://www.youtube.com/watch?v=VOHx7FdHE_0", loop=self.bot.loop)
+                ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
+
+    @commands.command(pass_context=True)
+    async def war(self, ctx):
+        try:
+            await self.joinDefault(ctx)
+            async with ctx.typing():
+                player = await YTDLSource.from_url("https://www.youtube.com/watch?v=5Ysj0wyTtsc", loop=self.bot.loop)
+                ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
+    
+    @commands.command(pass_context=True)
+    async def openTheCountry(self, ctx):
+        try:
+            await self.joinDefault(ctx)
+            async with ctx.typing():
+                player = await YTDLSource.from_url("https://www.youtube.com/watch?v=Cj1Tex6NfWQ", loop=self.bot.loop)
+                ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
+    
+    @commands.command(pass_context=True)
+    async def gimmie(self, ctx):
+        try:
+            await self.joinDefault(ctx)
+            async with ctx.typing():
+                player = await YTDLSource.from_url("https://www.youtube.com/watch?v=cfiPB8omuQQ", loop=self.bot.loop)
+                ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
+
+    @commands.command(pass_context=True, help="how bout you do it, anyway")
+    async def doitanyway(self, ctx):
+        try:
+            await self.joinDefault(ctx)
+            async with ctx.typing():
+                player = await YTDLSource.from_url("https://www.youtube.com/watch?v=yWULCfJ2PGA", loop=self.bot.loop)
+                ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            print("An exception occurred")
 
     @play.before_invoke
     @yt.before_invoke
